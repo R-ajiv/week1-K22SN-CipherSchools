9266289fa940c2b0ef6431549966eb72721087d8
Rajiv Kumar Singh <118833528+R-ajiv@users.noreply.github.com>
2023-10-08 02:05:58 +0530

    Lec_7

---
+++ Lecture_7.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // int password;
+
+    // do
+    // {
+    //     cin>>password;
+    // } 
+    // while (password<999999);
+
+    // int password;
+    // int count=0;
+
+    // while (1)
+    // {
+    //     cout<<"Chocolates ";
+    //     count++;
+
+    //     if(count>100) break;
+    // }
+    
+
+    // cout<<"Enter the password: "<<endl;
+    // cin>>password;
+
+    // while(password<999999)
+    // {
+    //     cout<<"The password does not meet the required conditions,Please enter the password again";
+    //     cin>>password;
+    // }
+
+    // cout<<"The User has now entered a correct password"<<endl;
+
+
+    int i;
+    for ( i = 0; ; i++)
+    {
+        cout<<i<<" ";
+        if(i>100) break;
+    }
+    
+    return 0;
+}
