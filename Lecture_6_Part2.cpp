44df75937b683d3d2bce200d5ace947ce1f7c46f
Rajiv Kumar Singh <118833528+R-ajiv@users.noreply.github.com>
2023-10-08 01:38:02 +0530

    Lec_6

---
+++ Lecture_6_Part2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // int i,j,k;
+    // i=0;
+    // j=0;
+    // k=0;
+
+    // cout<<i<<" "<<j<<" "<<k<<endl;
+    // j=i++;
+
+    // cout<<i<<" "<<j<<" "<<k<<endl;
+    // k=++i;
+
+    int i;
+
+    for(i=1;i<101;i++)
+    {
+        cout<<i<<" ";
+    }
+
+}
