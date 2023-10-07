be5d039a71419ac3515ab753ccedeb9ac2755507
Rajiv Kumar Singh <118833528+R-ajiv@users.noreply.github.com>
2023-10-07 22:36:16 +0530

    Lec_4

---
+++ Lecture_4_Part2.cpp
@@ -0,0 +1,95 @@
+#include<iostream>
+using namespace std;
+
+// // int main()
+// // {
+// //     float a,b;
+
+// //     cout<<"Give 2 floating point numbers as input"<<endl;
+// //     cin>>a>>b;
+
+// //     // if(a>0.01 && b>a)
+// //     if(a>0.01 || b>a)
+// //     {
+// //         cout<<"The condition in if statement is correct";
+// //     }
+// //     else{
+// //         cout<<"The condition is false";
+// //     }
+// // }
+
+
+// // int main(){
+// //     int marks;
+// //     cout<<"Student, please enter the marks to calculate the grade : ";
+// //     cin>>marks;
+
+// //     if(marks>90)
+// //     {
+// //         cout<<"A";
+// //     }
+// //     else if(marks>80)
+// //     {
+// //         cout<<"B";
+// //     }
+// //     else if(marks>70)
+// //     {
+// //         cout<<"C";
+// //     }
+// //     else
+// //     {
+// //         cout<<"Pass";
+// //     }
+
+// // }
+
+
+// // int main()
+// // {
+// //     int a,b,c,d;
+// //     cout<<"Give three integers as input : a,b,c"<<endl;
+// //     cin>>a>>b>>c>>d;
+
+// //     if(a>b && c>d)
+// //     {
+// //         cout<<"haha";
+// //     }
+// //     else if(a>b && c<d)
+// //     {
+// //         cout<<"hehe";
+// //     }
+// //     else
+// //     {
+// //         cout<<"huhu";
+// //     }
+
+
+// // }
+
+
+int main()
+{
+    int a;
+    cout<<"enter a integer a "<<endl;
+    cin>>a;
+
+    switch (a)
+    {
+    case 1:
+    cout<<"The value of a is 1"<<endl;
+        break;
+
+    case 2:
+    cout<<"The value of b is 2"<<endl;
+        break;
+
+    case 3:
+    cout<<"The value of c is 3"<<endl;
+    break;
+
+    default:
+    cout<<"default will be always printed";
+        break;
+    }
+}
+
